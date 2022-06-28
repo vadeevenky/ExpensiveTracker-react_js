b801eaea3d7243f58e2ee9d06ed40e4141edca50
Venky <106900726+vadeevenky@users.noreply.github.com>
2022-06-28 16:31:00 +0530

    Adding basic component (Expensiveitem) to app component

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExpensiveItem from "./components/ExpensiveItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's start React JS</h1>
+      <ExpensiveItem></ExpensiveItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ ExpensiveItem.js
@@ -0,0 +1,13 @@
+function ExpensiveItem() {
+  return (
+    <div>
+      <h2>18-10-1993</h2>
+      <div>
+        <h3>Land inviste</h3>
+        <div>$56.90</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpensiveItem;
