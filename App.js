30465298dfa78608c19c68a1a2cf03dd423b386d
Venky <106900726+vadeevenky@users.noreply.github.com>
2022-06-28 17:45:43 +0530

    concept of props in react js
    adding list of objects in app.js and using props and pass data from app.js to expensiveitem.js

--- App.js
+++ App.js
@@ -2,10 +2,36 @@ import "./App.css";
 import ExpensiveItem from "./components/ExpensiveItem";
 
 function App() {
+  const expenses = [
+    { title: "car Insurence", date: new Date(), price: 23.14 },
+    { title: "Land Investemnt", date: new Date(), price: 90.79 },
+    { title: "Home construct", date: new Date(), price: 34.09 },
+    { title: "bought gold", date: new Date(), price: 10.24 },
+  ];
+
   return (
     <div className="App">
       <h1>Let's start React JS</h1>
-      <ExpensiveItem></ExpensiveItem>
+      <ExpensiveItem
+        title={expenses[0].title}
+        date={expenses[0].date}
+        amount={expenses[0].price}
+      ></ExpensiveItem>
+      <ExpensiveItem
+        title={expenses[1].title}
+        date={expenses[1].date}
+        amount={expenses[1].price}
+      ></ExpensiveItem>
+      <ExpensiveItem
+        title={expenses[2].title}
+        date={expenses[2].date}
+        amount={expenses[2].price}
+      ></ExpensiveItem>
+      <ExpensiveItem
+        title={expenses[3].title}
+        date={expenses[3].date}
+        amount={expenses[3].price}
+      ></ExpensiveItem>
     </div>
   );
 }
--- ExpensiveItem.js
+++ ExpensiveItem.js
@@ -1,16 +1,16 @@
 import "./ExpensiveItem.css";
 
-function ExpensiveItem() {
-  const expensiveDate = new Date(2022, 12, 29);
-  const expensiveName = "car insurence";
-  const expensivePrice = "92.56";
+function ExpensiveItem(props) {
+  //   const expensiveDate = new Date(2022, 12, 29);
+  //   const expensiveName = "car insurence";
+  //   const expensivePrice = "92.56";
 
   return (
     <div className="expense-item">
-      <div>{expensiveDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expensiveName}</h2>
-        <div className="expense-item_price"> ${expensivePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item_price"> ${props.amount}</div>
       </div>
     </div>
   );
