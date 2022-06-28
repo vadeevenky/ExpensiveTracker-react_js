01a0a51a414a9fc727058f523e8a4023805499ec
Venky <106900726+vadeevenky@users.noreply.github.com>
2022-06-28 17:29:08 +0530

    adding Const variables and used in curly brackets  (jsx expersion)

--- ExpensiveItem.js
+++ ExpensiveItem.js
@@ -1,10 +1,16 @@
+import "./ExpensiveItem.css";
+
 function ExpensiveItem() {
+  const expensiveDate = new Date(2022, 12, 29);
+  const expensiveName = "car insurence";
+  const expensivePrice = "92.56";
+
   return (
-    <div>
-      <h2>18-10-1993</h2>
-      <div>
-        <h3>Land inviste</h3>
-        <div>$56.90</div>
+    <div className="expense-item">
+      <div>{expensiveDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expensiveName}</h2>
+        <div className="expense-item_price"> ${expensivePrice}</div>
       </div>
     </div>
   );
